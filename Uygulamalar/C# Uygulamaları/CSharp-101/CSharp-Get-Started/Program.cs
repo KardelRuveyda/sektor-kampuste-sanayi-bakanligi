8775d4b5957a6429bebe47b0d5868f955db40d42
Kardel Ruveyda <33912144+KardelRuveyda@users.noreply.github.com>
2023-12-01 21:13:07 +0300

    Update Program.cs

--- Uygulamalar/C# Uygulamaları/CSharp-101/CSharp-Get-Started/Program.cs
+++ Uygulamalar/C# Uygulamaları/CSharp-101/CSharp-Get-Started/Program.cs
@@ -319,5 +319,8 @@ string[] carsNew = new string[] { "Volvo", "BMW", "Ford", "Mazda", "Nissan" };
 //new anahtar sözcüğünü kullanamdan dört elemanlı bir dizi oluşturma 
 string[] carsNew2 = { "Volvo", "BMW", "Mazda", "Ford", "Nissan" };
 
+string[] carsNew4 = { "Volvo", "BMW", "Mazda", "Ford", "Nissan"};
+
+
 
 #endregion
