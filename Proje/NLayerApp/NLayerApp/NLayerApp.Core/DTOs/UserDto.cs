1607c5702df72d16b5e2a40d4a540a24ab0f6d49
Ruveyda Cetin <{ID}+{username}@users.noreply.github.com>
2024-12-16 11:22:10 +0300

    DTO lar oluşturuldu.

---
+++ Proje/NLayerApp/NLayerApp/NLayerApp.Core/DTOs/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core.DTOs
+{
+    public class UserDto:BaseDto
+    {
+        public string UserName { get; set; } = "";
+    }
+}
