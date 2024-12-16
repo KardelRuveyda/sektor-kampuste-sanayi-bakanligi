96537b92d04783243795d56576390d28e93a34e1
Ruveyda Cetin <{ID}+{username}@users.noreply.github.com>
2024-12-16 11:15:18 +0300

    Katmanlı mimari proje geliştirme çalışmaları

---
+++ Proje/NLayerApp/NLayerApp/NLayerApp.Core/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core.Models
+{
+    public class Team:BaseEntity
+    {
+        public string TeamName { get; set; } = "";
+    }
+}
---
+++ Proje/NLayerApp/NLayerApp/NLayerApp.Core/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core.Models
+{
+    public class User: BaseEntity
+    {
+        public string UserName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
