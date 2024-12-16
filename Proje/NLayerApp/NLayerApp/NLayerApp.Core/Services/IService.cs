0c856ce12da1b80f24384490338510f2240e3dd7
Ruveyda Cetin <{ID}+{username}@users.noreply.github.com>
2024-12-16 11:48:23 +0300

    Unitofwork ve servis işlemleri

---
+++ Proje/NLayerApp/NLayerApp/NLayerApp.Core/Services/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core.Services
+{
+    public interface IService<in T> where T : class
+    {
+        Task AddAsync();
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ Proje/NLayerApp/NLayerApp/NLayerApp.Core/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using NLayerApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core.Services
+{
+    internal interface IUserService:IService<User>
+    {
+    }
+}
